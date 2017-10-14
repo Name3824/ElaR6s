7fae83f59be5551645c7cc1e9210224f9d5ee374
PillGP <pill@apydle.com>
2017-10-14 11:02:21 +0100

    some changes

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ exports.run = (client, msg, args) => {
         },
         fields: [
             {
-                name: "Howdy!",
+                name: "**Howdy!**",
                 value: "I'm Gaming Bot, and as the name says, i'm a damn Gaming Bot!\nI'm built with [node.js](https://nodejs.org), [discord.js](https://discord.js.org) and with "+client.users.get("216901800899510272").tag+"'s love aswell!\nIf you want to add me to your guild to suite your gaming needs, click [here](https://discordapp.com/oauth2/authorize?client_id=357164743489880074&scope=bot&permissions=281664) to invite me!\n*[You need the `MANAGE_SERVER` permission to add me to a guild of your choice]*"
             },{
                 name: "Some of my commands:",
