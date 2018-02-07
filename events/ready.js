db07e2dc05eb2abfcce373c60d7a3468f62f04ad
PillGP <gaming.bot.discord@gmail.com>
2018-02-07 23:45:23 +0000

    :sparkles: | Changed how playing works

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 exports.run = (client) => {
     console.log('Preparing...');
-    client.user.setActivity(process.env.PREFIX+"help", {type: 'WATCHING'});
+    client.user.setActivity(process.env.PLAYING, {type: 'WATCHING'});
     console.log('Loading users... '+client.users.size+'\nLoading channels... '+client.channels.size+'\nLoading servers... '+client.guilds.size+'\n'+client.user.id+' is successfully online!');
 }
