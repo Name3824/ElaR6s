88dc39362d5e6a3f38939a6cc2032a803a784d69
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 18:25:53 +0100

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -1,3 +1,4 @@
+
 const Discord = require("discord.js");
 const request = require('snekfetch');
 const fs = require("fs")
@@ -11,9 +12,10 @@ exports.run = (client, message, args) => {
         message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
     } else {   
         var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
-        var embed = new Discord.RichEmbed()
-        .setImage("http://media.oboobs.ru/boobs_preview/" + MathLoL + ".jpg")
-            message.channel.sendMessage({embed});
+        request.get("http://media.oboobs.ru/boobs_preview/" + MathLoL + ".jpg").then(r => {
+            fs.writeFile(`${randomname}.jpg`, r.body)
+            message.channel.sendFile(r.body).then(d => {
+                fs.unlink(`./${randomname}.jpg`)
             })
         })
     }
