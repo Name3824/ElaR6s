86ab42c5c48dc106877cbb38b4ef2e61cff54249
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 15:25:52 +0100

    Update r6s.js

--- commands/r6s.js
+++ commands/r6s.js
@@ -1,149 +1,77 @@
 const RainbowSix = require('rainbowsix-api-node');
 const r6 = new RainbowSix();
+const platforms = ['PC', 'XBOX', 'PS4'];
+const Discord = require('discord.js');
+function platform(query) {
+    if(query == 'PC') return '1';
+    else if(query == 'PS4') return '2';
+    else if(query == 'XBOX') return '3';
+}
 
-exports.exec = (Bastion, message, args) => {
-  if (!/^(uplay|ps4|xone)$/.test(args[0] = args[0].toLowerCase())) {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'invalidInput'), Bastion.strings.error(message.guild.language, 'invalidPlatform', true, '`Uplay`, `PS4` and `XOne`'), message.channel);
-  }
-  if (!/^[a-zA-Z][\w-. ]{2,14}$/.test(args[1] = args.slice(1).join(' '))) {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'invalidInput'), Bastion.strings.error(message.guild.language, 'invalidInput', true, 'username'), message.channel);
-  }
-
-  r6.stats(args[1], args[0]).then(data => {
-    let stats = [
-      {
-        name: 'Player Name',
-        value: data.player.username
-      },
-      {
-        name: 'Level',
-        value: `${data.player.stats.progression.level}`,
-        inline: true
-      },
-      {
-        name: 'XP',
-        value: `${data.player.stats.progression.xp}`,
-        inline: true
-      }
-    ];
-    if (data.player.stats.ranked.has_played) {
-      stats.push(
-        {
-          name: 'Ranked',
-          value: `${args[1]} has played Ranked games for **${(data.player.stats.ranked.playtime / 60 / 60).toFixed(2)}** Hours.`
-        },
-        {
-          name: 'Wins',
-          value: `${data.player.stats.ranked.wins}`,
-          inline: true
-        },
-        {
-          name: 'Losses',
-          value: `${data.player.stats.ranked.losses}`,
-          inline: true
-        },
-        {
-          name: 'Kills',
-          value: `${data.player.stats.ranked.kills}`,
-          inline: true
-        },
-        {
-          name: 'Deaths',
-          value: `${data.player.stats.ranked.deaths}`,
-          inline: true
-        },
-        {
-          name: 'Win/Lose Ratio',
-          value: `${data.player.stats.ranked.wlr}`,
-          inline: true
-        },
-        {
-          name: 'Kill/Death Ratio',
-          value: `${data.player.stats.ranked.kd}`,
-          inline: true
+exports.run = (client, msg, args) => {
+    const emb = new Discord.RichEmbed();
+    const strings = args.join(" ");
+    if(strings.startsWith('-player')) {
+        playerdata = strings.replace('-player', '').trim();
+        playerData = playerdata.split(" ");
+        if(!playerData[0]) {
+            msg.channel.startTyping();
+            emb.setColor('#F03A17');
+            emb.addField('Platform not defined', 'Try again with a valid platform');
+            emb.setFooter(msg.author.tag, msg.author.avatarURL);
+            msg.channel.send({embed:emb});
+            msg.channel.stopTyping();
+        } else if(!platforms.includes(playerData[0].toUpperCase())) {
+            msg.channel.startTyping();
+            emb.setColor('#F03A17');
+            emb.addField('Invalid platform', 'Try again with a valid platform');
+            emb.setFooter(msg.author.tag, msg.author.avatarURL);
+            msg.channel.send({embed:emb});
+            msg.channel.stopTyping();
+        } else if(!playerData[1]) {
+            msg.channel.startTyping();
+            emb.setColor('#F03A17');
+            emb.addField('Username not defined', 'Try again with a valid username');
+            emb.setFooter(msg.author.tag, msg.author.avatarURL);
+            msg.channel.send({embed:emb});
+            msg.channel.stopTyping();
+        } else if(platforms.includes(playerData[0].toUpperCase())) {
+            platf = playerData[0];
+            plat = platform(platf.toUpperCase());
+            player = playerdata.replace(platf, '').trim();
+            rocket.getPlayer(player, plat, async function (status, data) {
+                if(status === 200) {
+                    await msg.channel.startTyping();
+                    await emb.setColor('#0592DF');
+                    await emb.setAuthor('Stats about '+data.player.username, 'https://vignette.wikia.nocookie.net/rocketleague/images/f/f6/Rocketleague-logo.png/revision/latest?cb=20161207070401', 'https://rocketleaguestats.com/');
+                    await emb.setFooter(msg.author.tag, msg.author.avatarURL);
+                    await msg.channel.send({embed:emb});
+                    await msg.channel.stopTyping();
+                } else if(status === 404) {
+                    await msg.channel.startTyping();
+                    await emb.setColor('#F03A17');
+                    await emb.addField('User not found', 'Try again with a valid username / tag');
+                    await emb.setFooter(msg.author.tag, msg.author.avatarURL);
+                    await msg.channel.send({embed:emb});
+                    await msg.channel.stopTyping();
+                } else {
+                    await msg.channel.startTyping();
+                    await emb.setColor('#F03A17');
+                    await emb.addField('An error has occured', 'Report this to '+client.users.get(process.env.OWNER).tag+' with the code '+status);
+                    await emb.setFooter(msg.author.tag, msg.author.avatarURL);
+                    await msg.channel.send({embed:emb});
+                    await msg.channel.stopTyping();
+                }
+            });
         }
-      );
+    } else if(!args[0]) {
+        msg.channel.startTyping();
+        emb.setColor('#0592DF');
+        emb.setAuthor('Rocket League Commands [powered by RocketLeagueStats]', 'https://vignette.wikia.nocookie.net/rocketleague/images/f/f6/Rocketleague-logo.png/revision/latest?cb=20161207070401', 'https://rocketleaguestats.com/');
+        emb.setThumbnail('https://i.imgur.com/HzLyjWn.png');
+        emb.addField('`-player`', "Search for a player's stats\nUsage: `"+process.env.PREFIX+"rocket -player [platform] [username]`\n\nValid platforms: `pc, ps4, xbox`\nIf you're checking stats on the PC, use your [STEAM64 ID](https://steamid.io/)");
+        emb.setFooter(msg.author.tag, msg.author.avatarURL);
+        msg.channel.send({embed:emb});
+        msg.channel.stopTyping();
     }
-    else {
-      stats.push(
-        {
-          name: 'Ranked',
-          value: `${args[1]} has not played any Ranked game.`
-        }
-      );
-    }
-    if (data.player.stats.casual.has_played) {
-      stats.push(
-        {
-          name: 'Casual',
-          value: `${args[1]} has played Casual games for **${(data.player.stats.casual.playtime / 60 / 60).toFixed(2)}** Hours.`
-        },
-        {
-          name: 'Wins',
-          value: `${data.player.stats.casual.wins}`,
-          inline: true
-        },
-        {
-          name: 'Losses',
-          value: `${data.player.stats.casual.losses}`,
-          inline: true
-        },
-        {
-          name: 'Kills',
-          value: `${data.player.stats.casual.kills}`,
-          inline: true
-        },
-        {
-          name: 'Deaths',
-          value: `${data.player.stats.casual.deaths}`,
-          inline: true
-        },
-        {
-          name: 'Win/Lose Ratio',
-          value: `${data.player.stats.casual.wlr}`,
-          inline: true
-        },
-        {
-          name: 'Kill/Death Ratio',
-          value: `${data.player.stats.casual.kd}`,
-          inline: true
-        }
-      );
-    }
-    else {
-      stats.push(
-        {
-          name: 'Casual',
-          value: `${args[1]} has not played any Casual game.`
-        }
-      );
-    }
-    message.channel.send({
-      embed: {
-        color: 0xffffff,
-        title: 'Rainbow 6',
-        url: `https://r6stats.com/stats/${args[0]}/${encodeURIComponent(args[1])}`,
-        fields: stats,
-        thumbnail: {
-          url: 'https://vignette1.wikia.nocookie.net/rainbowsix/images/0/06/Rainbow_(Clear_Background)_logo.png'
-        }
-      }
-    }).catch(e => {
-      Bastion.log.error(e);
-    });
-  }).catch(() => {
-    /**
-     * Error condition is encountered.
-     * @fires error
-     */
-    return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'notFound'), Bastion.strings.error(message.guild.language, 'notFound', true, 'player'), message.channel);
-  });
-};
+}
