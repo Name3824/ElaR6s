bacd8e627d865c7641238bf69a4e10211d91132c
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 19:31:06 +0100

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -2,7 +2,7 @@ const snek = require("snekfetch");
 
 exports.run = async (client, message, args) => {
     try {
-      if (!message.channel.nsfw) return message.reply("🔞 Cannot display NSFW content in a SFW channel.");
+      if (!message.channel.nsfw) return message.reply("🔞 You are not in NSFW channel.");
       const msg = await message.channel.send(`**${message.member.displayName}** is looking for a feline...`);
       const { body } = await snek.get(`https://nekos.life/api${Math.random() >= 0.5 ? "/lewd" : ""}/neko`);
       message.channel.send({ embed: { image: { url: body.neko } } });
