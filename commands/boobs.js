ecbf1fb851b794fadc8ecb4fdbbbafd537471b63
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 19:30:16 +0100

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -9,7 +9,11 @@ exports.run = (client, message, args) => {
     var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
     var MathLoL = Math.round(MathRan);
     if (!message.channel.nsfw) {
-        message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+        const embed = new Discord.RichEmbed()
+        .setTitle("Error.")
+        .setDescription("🔞 You are not in NSFW channel")
+        .setColor(0xf94a53)
+message.channel.send({embed})
     } else {   
         var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
         request.get("http://media.oboobs.ru/boobs_preview/" + MathLoL + ".jpg").then(r => {
