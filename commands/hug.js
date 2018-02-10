55652fcd9b478df4afe840d7be69d0eae9e77610
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 14:56:01 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,4 +1,4 @@
-    exports.run = (client, msg, args) => {
+    exports.run = (client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.get(args) || message.member
     let patlinks = ["http://media3.giphy.com/media/14aBJO7py75MD6/giphy.gif", "http://gifimage.net/wp-content/uploads/2017/07/anime-tackle-hug-gif-9.gif", "http://i.imgur.com/tuH4gqZ.gif", "https://68.media.tumblr.com/f5b4d602384bdaac19f8fcba5d12359d/tumblr_opo7uiZD8H1w169g0o1_500.gif", "https://pa1.narvii.com/6472/0bb16033352529eaa6eec73a63f347967e330c62_hq.gif"]
     let randompat = patlinks[Math.floor(Math.random() * patlinks.length)]
@@ -10,5 +10,7 @@
             title: `${message.author.username} just hug ${member.user.username}!💜`,
             image: {
                 url: `${randompat}`
-       }
-      }
+            }
+        }})
+    }
+}
