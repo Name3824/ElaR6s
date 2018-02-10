a48de6b9c5cd9c657c192305b4df2729a3255517
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-10 16:54:51 +0100

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -42,9 +42,9 @@ exports.run = (client, msg, args) => {
             player = fullStats.replace(platf, '').trim();
             Fortnite.get(player, plat).then(async function (p) {
                 await msg.channel.startTyping();
-                await emb.setAuthor('Stats about '+p.displayName, 'https://res.cloudinary.com/teepublic/image/private/s--W2EuKnua--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1511850332/production/designs/2112154_1.jpg');
+                await emb.setAuthor('Stats about '+p.displayName, 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
                 await emb.setColor('#FBD535');
-                await emb.setThumbnail('https://res.cloudinary.com/teepublic/image/private/s--W2EuKnua--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1511850332/production/designs/2112154_1.jpg');
+                await emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
                 await emb.addField('General', `**Score**: ${p.stats.score}\n**Kills**: ${p.stats.kills}\n**Matches**: ${p.stats.matches}\n**Top 1**: ${p.stats.top1}\n**Top 3**: ${p.stats.top3}\n**Top 5**: ${p.stats.top5}\n**Top 25**: ${p.stats.top25}\n**K/D**: ${p.stats.kd}\n**Time Played**: ${p.stats.timePlayed}`);
                 await emb.setFooter(msg.author.tag, msg.author.avatarURL);
                 await msg.channel.send({embed:emb});
@@ -54,8 +54,8 @@ exports.run = (client, msg, args) => {
     } else if(!args[0]) {
         msg.channel.startTyping();
         emb.setColor('#FBD535');
-        emb.setAuthor('Fortnite Commands', 'https://res.cloudinary.com/teepublic/image/private/s--W2EuKnua--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1511850332/production/designs/2112154_1.jpg');
-        emb.setThumbnail('https://res.cloudinary.com/teepublic/image/private/s--W2EuKnua--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1511850332/production/designs/2112154_1.jpg');
+        emb.setAuthor('Fortnite Commands', 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
+        emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
         emb.addField('`-player`', "Search for a player's stats\nUsage: `"+process.env.PREFIX+"fortnite -player [platform] [username]`\n\nValid platforms: `pc, ps4, xbox`");
         emb.setFooter(msg.author.tag, msg.author.avatarURL);
         msg.channel.send({embed:emb});
