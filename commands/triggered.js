6a58cef2df074ebf04254e828c8e1820056afbf0
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 13:25:21 +0100

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -1,7 +1,7 @@
   const snekfetch = require('snekfetch');
   exports.run = async (client, message, args) => {
     message.mentions.users.first() !== undefined ? person = message.mentions.users.first() : person = message.author
-    const avatar = message.mentions.users.first().displayAvatarURL({format : 'png'})
+    const avatar = person.user.avatarURL({format : 'png'})
     const pls = await snekfetch.get(`http://api.anidiots.guide/api/triggered/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
     await message.channel.send({ files: [{ attachment: pls.body, name: 'triggered.gif' }] });
 } 
