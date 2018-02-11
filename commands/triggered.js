0e8e4b42dd328f666f04f8b3084bbd512004e933
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 13:31:01 +0100

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -2,6 +2,7 @@
    const Discord = require('discord.js');
   exports.run = async (client, message, args) => {
     const mentioned = message.mentions.users.first()
+    if (!mentioned) {
     const avatar = mentioned.displayAvatarURL({format : 'png'})
     const pls = await snekfetch.get(`http://api.anidiots.guide/api/triggered/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
     await message.channel.send({ files: [{ attachment: pls.body, name: 'triggered.gif' }] });
