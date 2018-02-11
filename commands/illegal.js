63ff5b922a04f5e448e46072ec6d5e090a3d77fb
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:24:54 +0100

    Create illegal.js

---
+++ commands/illegal.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const { get, post } = require('snekfetch');
+exports.run = async (client, message, args) => {
+let word = args[0].toUpperCase().split('')
+if(word.length > 10) return message.reply('Must be less than 10 characters')
+if(word.length < 1) return message.reply('Must be more than 1 character.')     
+for(i in word) {
+  if(!isNaN(word[i])) {
+    return message.reply('You cannot use numbers.')
+  }
+}
+const embed = new Discord.MessageEmbed()
+.setAuthor(`Trump has now made ${args[0]} illegal!`)
+.setImage(`https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/${word.join('')}.gif`)
+.setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86)
+await post('https://is-now-illegal.firebaseio.com/queue/tasks.json').send({ task: 'gif', word: args[0]}).then(message.channel.send(`Waiting for Trump's approvement to make ${args[0]} illegal.`)).then(setTimeout(function() {
+  message.channel.send(embed);
+}, 10000))
+
+};
