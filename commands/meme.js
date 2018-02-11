c10b55d69175e7b6e4e94fd0c3b9b1c607a4dd53
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 13:07:07 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -4,9 +4,9 @@ exports.run = (client, message, args) => {
         memes('day', 100, function(err, data) {
             JSON.stringify(data)
             var result = Math.floor((Math.random() * 100));
-            const embed = new Discord.MessageEmbed()
-                .setImage(data[result])
-                .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
-            message.channel.send(embed)
+            const emb = new Discord.MessageEmbed()
+                emb.setImage(data[result]);
+                emb.setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
+            message.channel.send({embed:emb});
         });
 }
