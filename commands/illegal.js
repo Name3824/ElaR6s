a21e0835b4b47bbf6a2e024f4ac262fcda063281
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 17:07:55 +0100

    Update illegal.js

--- commands/illegal.js
+++ commands/illegal.js
@@ -4,9 +4,9 @@ if (message.channel.type === "dm") return;
 let meow = message.content.split(" ").slice(1);
  	let args1 = meow.join(' ');
 let illegal = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` + args1.toUpperCase() + `.gif`;
- if (!meow) { 
+ if (!args1) { 
 return message.reply(':x: **Please, specify a thing that trump will make illegal**'); }
-if (args1.length > 1) {
+if (meow.length > 1) {
 return message.reply(':x: **Max 1 word**'); }
  const emb = new Discord.RichEmbed();
  emb.setAuthor("Trump has now made ${meow} illegal!", "http://blog.adsy.me/wp-content/uploads/2016/11/angry-side-face-trump-transparent.png");
