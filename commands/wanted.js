c2c0df828e3bb8a278462b4776d98047844c527f
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:20:20 +0100

    Delete wanted.js

--- commands/wanted.js
+++
@@ -1,6 +0,0 @@
-const snekfetch = require('snekfetch');
-exports.run = (client, message, args) => {
-  const avatar = message.author.displayAvatarURL({format : 'png'})
-  const pls = await snekfetch.get(`http://api.anidiots.guide/api/wanted/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
-  await message.channel.send({ files: [{ attachment: pls.body, name: 'wanted.jpg' }] });
-}
