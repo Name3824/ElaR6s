9de9c2eb5de536e6004bf391341cdfe9adb08ad8
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:40:00 +0100

    Update illegal.js

--- commands/illegal.js
+++ commands/illegal.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
 if (message.channel.type === "dm") return;
-let args = message.content.split(" ").slice(1);
- 	let args1 = args.join(' ');
+let meow = message.content.split(" ").slice(1);
+ 	let args1 = meow.join(' ');
 let illegal = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` + args1.toUpperCase() + `.gif`;
- if (!args) { 
+ if (!meow) { 
 return message.reply(':x: **Please, specify a thing that trump will make illegal**'); }
-if (args.length > 1) {
+if (meow.length > 1) {
 return message.reply(':x: **Max 1 word**'); }
  message.channel.send({ files: [{ attachment: illegal, name: 'isnowillegal.gif' }] })
 }
