2e72ff5cff0b0ccfd0d1ba07c91e6226f2013699
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 13:59:56 +0100

    Create memeuser.js

---
+++ commands/memeuser.js
@@ -0,0 +1,19 @@
+exports.run = (client, msg, args) => {
+    const Discord = require('discord.js');
+    const [usr, top, bottom] = args.join(' ').split(' | ');
+    const user = msg.mentions.users.first();
+    if (!usr && !top && !bottom) return msg.reply('Command cannot be ran like this, problem between keyboard and chair.');
+    if (!usr) return msg.reply('Invalid, no user defined.');
+    if (!top) return msg.reply('Invalid, top text missing.');
+    if (!bottom) return msg.reply('Invalid, bottom text missing.');
+
+
+    const image = user.avatarURL();
+    const memeurl = `https://memegen.link/custom/${encodeURIComponent(top)}/${encodeURIComponent(bottom)}.jpg?alt=${encodeURIComponent(image)}&font=impact`;
+    const meme = new Discord.MessageEmbed()
+        .setImage(memeurl);
+
+    msg.channel.send({
+        embed: meme
+    });
+};
