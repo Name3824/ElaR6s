1c6b91ff7940fb63c9c945a80a3744151ac6d7f5
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 12:51:30 +0100

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js')
 var figlet = require('figlet');
 exports.run = (client, message, args) => {
     
-    const errorEmbed = new Discord.MessageEmbed()
-    .setAuthor('Error')
-    .setDescription('You must enter a phrase.')
-    .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
+  const emb = new Discord.RichEmbed();
+    emb.setAuthor('Error');
+    emb.setDescription('You must enter a phrase.');
+    emb.setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
 
         let asciiwords = args.join(' ');
-        if(!asciiwords) return message.channel.send(errorEmbed);
+        if(!asciiwords) return message.channel.send(emb);
         figlet(asciiwords, function(err, data) {
             if (err) {
                 client.log('Something went wrong...');
