b1ff6e709b87398940f5210468ab3e5457bc7d12
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:47:04 +0100

    Update illegal.js

--- commands/illegal.js
+++ commands/illegal.js
@@ -8,5 +8,8 @@ let illegal = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` 
 return message.reply(':x: **Please, specify a thing that trump will make illegal**'); }
 if (meow.length > 1) {
 return message.reply(':x: **Max 1 word**'); }
- message.channel.send({ files: [{ attachment: illegal, name: 'isnowillegal.gif' }] })
+ const emb = new Discord.RichEmbed();
+ emb.setAuthor("Meow");
+ emb.setImage(illegal);
+ message.channel.send({embed:emb})
 }
