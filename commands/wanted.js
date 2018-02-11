5c06ce557424c629f4b5bb1eb718bb1062307750
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:06:05 +0100

    Create wanted.js

---
+++ commands/wanted.js
@@ -0,0 +1,7 @@
+const snekfetch = require('snekfetch');
+exports.run = async (client, message, args) => {
+  message.mentions.users.first() !== undefined ? person = message.mentions.users.first() : person = message.author
+  const avatar = person.displayAvatarURL({format : 'png'})
+  const pls = await snekfetch.get(`http://api.anidiots.guide/api/wanted/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
+  await message.channel.send({ files: [{ attachment: pls.body, name: 'wanted.jpg' }] });
+}
