cfe17a8c3cf0e96de637677bffb12686e3db69e1
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-11 16:36:55 +0100

    Update illegal.js

--- commands/illegal.js
+++ commands/illegal.js
@@ -1,20 +1,12 @@
 const Discord = require('discord.js')
-const { get, post } = require('snekfetch');
 exports.run = async (client, message, args) => {
-let word = args.join('')
-if(word.length > 10) return message.reply('Must be less than 10 characters')
-if(word.length < 1) return message.reply('Must be more than 1 character.')     
-for(i in word) {
-  if(!isNaN(word[i])) {
-    return message.reply('You cannot use numbers.')
-  }
+if (message.channel.type === "dm") return;
+let args = message.content.split(" ").slice(1);
+ 	let args1 = args.join(' ');
+let illegal = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` + args1.toUpperCase() + `.gif`;
+ if (!args) { 
+return message.reply(':x: **Please, specify a thing that trump will make illegal**'); }
+if (args.length > 1) {
+return message.reply(':x: **Max 1 word**'); }
+ message.channel.send({ files: [{ attachment: illegal, name: 'isnowillegal.gif' }] })
 }
-const emb = new Discord.MessageEmbed()
-emb.setAuthor(`Trump has now made ${args[0]} illegal!`)
-emb.setImage(`https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/${word.join('')}.gif`)
-emb.setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86)
-await post('https://is-now-illegal.firebaseio.com/queue/tasks.json').send({ task: 'gif', word: args[0]}).then(message.channel.send(`Waiting for Trump's approvement to make ${args[0]} illegal.`)).then(setTimeout(function() {
-  message.channel.semb({embed:emb});
-}, 10000))
-
-};
