8c8fa8edd1848c1aa54a62933c563c87822224f4
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-12 17:58:16 +0100

    Update minecraft.js

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -4,12 +4,12 @@ const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
     const emb = new Discord.RichEmbed();
     const query = args.join(" ");
-    if(query.startsWith("-server")) {
-        ip = query.replace('-server', '').trim();
+    if(query.startsWith("server")) {
+        ip = query.replace('server', '').trim();
         if(!ip) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Minecraft server not valid.', 'Use a valid one, for example `mc.hypixel.net`');
+            emb.addField('Minecraft server not valid.', 'Example `mc.hypixel.net`');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
@@ -17,7 +17,7 @@ exports.run = (client, msg, args) => {
         var url = 'https://mcapi.de/api/image/favicon/'+ip
         ping.pc({ server: ip }).then(async function(data) {
             await msg.channel.startTyping();
-            await emb.setColor('#44FC37');
+            await emb.setColor("#75C0AC");
             await emb.setAuthor('Information about '+ip, url);
             await emb.addField('Version', data.version.name);
             await emb.addField('Protocol', data.version.protocol);
@@ -27,18 +27,18 @@ exports.run = (client, msg, args) => {
             await msg.channel.send({embed:emb});
             await msg.channel.stopTyping();
         }).catch(console.error);
-    } else if(query.startsWith("-user")) {
-        user = query.replace('-user', '').trim();
+    } else if(query.startsWith("user")) {
+        user = query.replace('user', '').trim();
         if(!user) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Minecraft user not valid.', 'Use a valid one, for example `Dinnerbone`');
+            emb.addField('Minecraft user not valid.', 'Example `TovaYolo_Bagnato`');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
         }
         msg.channel.startTyping();
-        emb.setColor('#44FC37');
+        emb.setColor("#75C0AC");
         emb.setAuthor(user+"'s Skin", "http://minecraft-skin-viewer.net/3d.php?layers=true&aa=true&a=0&w=0&wt=0&abg=0&abd=0&ajg=0&ajd=0&ratio=13&format=png&login="+user+"&headOnly=true&displayHairs=true&randomness=384");
         emb.setImage("http://minecraft-skin-viewer.net/3d.php?layers=true&aa=true&a=0&w=0&wt=0&abg=0&abd=0&ajg=0&ajd=0&ratio=13&format=png&login="+user+"&headOnly=false&displayHairs=true&randomness=384");
         emb.setFooter(msg.author.tag, msg.author.avatarURL);
@@ -46,11 +46,11 @@ exports.run = (client, msg, args) => {
         msg.channel.stopTyping();
     } else if(!args[0]) {
         msg.channel.startTyping();
-        emb.setColor('#44FC37');
-        emb.setAuthor('Minecraft Commands', 'https://media.discordapp.net/attachments/264445053596991498/366656518524895232/unknown.png', 'https://minecraft.net');
+        emb.setColor("#75C0AC");
+        emb.setAuthor('Correct Usage Of Minecraft Commands', 'https://media.discordapp.net/attachments/264445053596991498/366656518524895232/unknown.png', 'https://minecraft.net');
         emb.setThumbnail('https://media.discordapp.net/attachments/264445053596991498/366656518524895232/unknown.png');
-        emb.addField('`-user`', "Search for a user's skin\nUsage: `"+process.env.PREFIX+"minecraft -user [username]`");
-        emb.addField('`-server`', "Search for a server's status\nUsage: `"+process.env.PREFIX+"minecraft -server [server ip]`");
+        emb.addField("Search for a user's skin\nUsage:", "e!minecraft user [username]");
+        emb.addField("Search for a server's status\nUsage:", "e!minecraft server [server ip]");
         emb.setFooter(msg.author.tag, msg.author.avatarURL);
         msg.channel.send({embed:emb});
         msg.channel.stopTyping();
