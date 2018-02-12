39ca3faab9d4ae8eee70e800d67540f3e36284f0
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-12 19:55:04 +0100

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,14 +1,14 @@
    const Discord = require('discord.js');
     const translate = require('google-translate-api');    
-    exports.run = async (client, message, args) => {
+    exports.run = (client, message, args) => {
         let words = message.content.split(" ").slice(2).join(" ");
         let language = args[0]
         translate(words, {
             to: language
         }).then(res => {
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(words)
-                .setDescription(res.text)
-                .setFooter(res.from.language.iso)
-                .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
-   message.channel.send(embed)
+            const emb = new Discord.RichEmbed()
+                emb.setAuthor(words)
+                emb.setDescription(res.text)
+                emb.setFooter(res.from.language.iso)
+                emb.setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
+   message.channel.send({embed:emb})
