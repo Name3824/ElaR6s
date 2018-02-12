ed9f01f1f83715db6ecc7e9ece95f19963f937c3
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-12 17:46:12 +0100

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -12,27 +12,30 @@ function platform(query, fort) {
 exports.run = (client, msg, args) => {
     const q = args.join(" ");
     const emb = new Discord.RichEmbed();
-    if(q.startsWith('-player')) {
-        fullStats = q.replace('-player', '').trim();
+    if(q.startsWith('player')) {
+        fullStats = q.replace('player', '').trim();
         allStats = fullStats.split(" ");
         if(!allStats[0]) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Platform not defined', 'Try again with a valid platform');
+            emb.setTitle('Error.');
+            emb.addField('Platform not defined', 'Try again');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
         } else if(!platforms.includes(allStats[0].toUpperCase())) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Invalid platform', 'Try again with a valid platform');
+            emb.setTitle('Error.');
+            emb.addField('Invalid platform', 'Try again');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
         } else if(!allStats[1]) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Username not defined', 'Try again with a valid username');
+            emb.setTitle('Error.');
+            emb.addField('Username not defined', 'Try again with');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
@@ -43,7 +46,7 @@ exports.run = (client, msg, args) => {
             Fortnite.get(player, plat).then(async function (p) {
                 await msg.channel.startTyping();
                 await emb.setAuthor('Stats about '+p.displayName, 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-                await emb.setColor('#FBD535');
+                await emb.setColor("#75C0AC");
                 await emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
                 await emb.addField('General', `**Score**: ${p.stats.score}\n**Kills**: ${p.stats.kills}\n**Matches**: ${p.stats.matches}\n**Top 1**: ${p.stats.top1}\n**Top 3**: ${p.stats.top3}\n**Top 5**: ${p.stats.top5}\n**Top 25**: ${p.stats.top25}\n**K/D**: ${p.stats.kd}\n**Time Played**: ${p.stats.timePlayed}`);
                 await emb.setFooter(msg.author.tag, msg.author.avatarURL);
@@ -53,10 +56,10 @@ exports.run = (client, msg, args) => {
         }
     } else if(!args[0]) {
         msg.channel.startTyping();
-        emb.setColor('#FBD535');
-        emb.setAuthor('Fortnite Commands', 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
+        emb.setColor("#75C0AC");
+        emb.setAuthor('Correct Usage Of Fortnite Commands', 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
         emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-        emb.addField('`-player`', "Search for a player's stats\nUsage: `"+process.env.PREFIX+"fortnite -player [platform] [username]`\n\nValid platforms: `pc, ps4, xbox`");
+        emb.addField("Search for a player's stats\nUsage:" "e!fortnite player [platform] [username]\n\nValid platforms: `pc, ps4, xbox`");
         emb.setFooter(msg.author.tag, msg.author.avatarURL);
         msg.channel.send({embed:emb});
         msg.channel.stopTyping();
