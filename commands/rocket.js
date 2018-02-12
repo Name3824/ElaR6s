dbaebe20791be71a7ac09e138f21715e815f9ca1
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-12 19:33:46 +0100

    Update rocket.js

--- commands/rocket.js
+++ commands/rocket.js
@@ -11,27 +11,30 @@ function platform(query) {
 exports.run = (client, msg, args) => {
     const emb = new Discord.RichEmbed();
     const strings = args.join(" ");
-    if(strings.startsWith('-player')) {
-        playerdata = strings.replace('-player', '').trim();
+    if(strings.startsWith('player')) {
+        playerdata = strings.replace('player', '').trim();
         playerData = playerdata.split(" ");
         if(!playerData[0]) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Platform not defined', 'Try again with a valid platform');
+            emb.setTitle('Error.');
+            emb.addField('Platform not defined', 'Try agai');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
         } else if(!platforms.includes(playerData[0].toUpperCase())) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Invalid platform', 'Try again with a valid platform');
+            emb.setTitle('Error.');
+            emb.addField('Invalid platform', 'Try again');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
         } else if(!playerData[1]) {
             msg.channel.startTyping();
             emb.setColor('#F03A17');
-            emb.addField('Username not defined', 'Try again with a valid username');
+            emb.setTitle('Error.');
+            emb.addField('Username not defined', 'Try again');
             emb.setFooter(msg.author.tag, msg.author.avatarURL);
             msg.channel.send({embed:emb});
             msg.channel.stopTyping();
@@ -42,7 +45,7 @@ exports.run = (client, msg, args) => {
             rocket.getPlayer(player, plat, async function (status, data) {
                 if(status === 200) {
                     await msg.channel.startTyping();
-                    await emb.setColor('#0592DF');
+                    await emb.setColor("#75C0AC");
                     await emb.setAuthor('Stats about '+data.displayName, 'https://vignette.wikia.nocookie.net/rocketleague/images/f/f6/Rocketleague-logo.png/revision/latest?cb=20161207070401', 'https://rocketleaguestats.com/');
                     await emb.setImage(data.signatureUrl);
                     await emb.setFooter(msg.author.tag, msg.author.avatarURL);
@@ -51,13 +54,15 @@ exports.run = (client, msg, args) => {
                 } else if(status === 404) {
                     await msg.channel.startTyping();
                     await emb.setColor('#F03A17');
-                    await emb.addField('User not found', 'Try again with a valid username / tag');
+                    await emb.setTitle('Error.');
+                    await emb.addField('User not found', 'Try again');
                     await emb.setFooter(msg.author.tag, msg.author.avatarURL);
                     await msg.channel.send({embed:emb});
                     await msg.channel.stopTyping();
                 } else {
                     await msg.channel.startTyping();
                     await emb.setColor('#F03A17');
+                    await emb.setTitle('Error.');
                     await emb.addField('An error has occured', 'Report this to '+client.users.get(process.env.OWNER).tag+' with the code '+status);
                     await emb.setFooter(msg.author.tag, msg.author.avatarURL);
                     await msg.channel.send({embed:emb});
@@ -67,10 +72,10 @@ exports.run = (client, msg, args) => {
         }
     } else if(!args[0]) {
         msg.channel.startTyping();
-        emb.setColor('#0592DF');
-        emb.setAuthor('Rocket League Commands [powered by RocketLeagueStats]', 'https://vignette.wikia.nocookie.net/rocketleague/images/f/f6/Rocketleague-logo.png/revision/latest?cb=20161207070401', 'https://rocketleaguestats.com/');
+        emb.setColor("#75C0AC");
+        emb.setAuthor('Correct Usage of Rocket League Commands', 'https://vignette.wikia.nocookie.net/rocketleague/images/f/f6/Rocketleague-logo.png/revision/latest?cb=20161207070401', 'https://rocketleaguestats.com/');
         emb.setThumbnail('https://i.imgur.com/HzLyjWn.png');
-        emb.addField('`-player`', "Search for a player's stats\nUsage: `"+process.env.PREFIX+"rocket -player [platform] [username]`\n\nValid platforms: `pc, ps4, xbox`\nIf you're checking stats on the PC, use your [STEAM64 ID](https://steamid.io/)");
+        emb.addField("Search for a player's stats\nUsage:" +process.env.PREFIX+ "rocket player [platform] [username]`\n\nValid platforms: `pc, ps4, xbox`");
         emb.setFooter(msg.author.tag, msg.author.avatarURL);
         msg.channel.send({embed:emb});
         msg.channel.stopTyping();
