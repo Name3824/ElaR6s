7ab06e2e68827d72152f3a335ac7cadb8ca30501
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-14 18:29:24 +0100

    Update beautiful.js

--- commands/beautiful.js
+++ commands/beautiful.js
@@ -2,6 +2,7 @@ const snekfetch = require('snekfetch');
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   const person = message.mentions.users.first()
+   if (!person) {
   const avatar = person.displayAvatarURL({format : 'png'})
   const pls = snekfetch.get(`http://api.anidiots.guide/api/beautiful/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
   message.channel.send({ files: [{ attachment: pls.body, name: 'beautiful.jpg' }] });
