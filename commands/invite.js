64254e68befbbb2d323b970dc101e0f698518c38
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-14 16:16:45 +0100

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+exports.run = (client, msg) => {
+    const Discord = require('discord.js');
+    const emb = new Discord.RichEmbed();
+    msg.channel.startTyping();
+    emb.setColor("#75C0AC");
+    emb.setAuthor(client.user.tag+' invite', client.user.avatarURL, 'http://mitsuhabot.ml');
+    emb.addField('**Invite Me!**', '[Click Me](https://discordapp.com/oauth2/authorize?client_id=411860199473938433&scope=bot&permissions=8)', true);
+    emb.setFooter('Prefix e! | e!partnerbot');
+    msg.channel.send({embed:emb});
+    msg.channel.stopTyping();
+}
