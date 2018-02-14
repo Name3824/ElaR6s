585f0ef4491d03b5ee094b91e28de8e8a275bace
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-14 09:25:00 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -11,13 +11,14 @@ exports.run = (client, msg) => {
     const emb = new Discord.RichEmbed();
     msg.channel.startTyping();
     emb.setColor("#75C0AC");
-    emb.setAuthor('Stats of '+client.user.tag, client.user.avatarURL, 'http://vgh.ftp.sh');
+    emb.setAuthor('Stats of '+client.user.tag, client.user.avatarURL, 'http://mitsuhabot.ml');
     emb.addField('Servers', client.guilds.size, true);
     emb.addField('Channels', client.channels.size, true);
     emb.addField('Users', client.users.size, true);
     emb.addField('Discord.js Version', Discord.version, true);
     emb.addField('Node.js Version', process.version, true);
     emb.addField('PID', process.pid, true);
+    emb.addField('Uptime', (Math.round(client.uptime / (1000 * 60 * 60))) + " hours, " + (Math.round(client.uptime / (1000 * 60)) % 60) + " minutes, and " + (Math.round(client.uptime / 1000) % 60) + " seconds"), true);
     emb.addField('Process Platform', platform(process.platform), true);
     emb.setThumbnail(client.user.avatarURL);
     msg.channel.send({embed:emb});
