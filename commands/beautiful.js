130d3add57d93045e2b45d57f18592b5b9565e1b
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-14 18:35:16 +0100

    Update beautiful.js

--- commands/beautiful.js
+++ commands/beautiful.js
@@ -1,10 +1,22 @@
-const snekfetch = require('snekfetch');
-const Discord = require('discord.js');
-exports.run = (client, message, args) => {
-  const person = message.mentions.users.first()
-   if (!person) {
-  const avatar = person.displayAvatarURL({format : 'png'})
-  const pls = snekfetch.get(`http://api.anidiots.guide/api/beautiful/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
-  message.channel.send({ files: [{ attachment: pls.body, name: 'beautiful.jpg' }] })
-   }
+const { Canvas } = require('canvas-constructor');
+const fsn = require('fs-nextra');
+exports.run = async (client, message, args) => {
+const getBeautiful = async (person) => {
+  const plate = await fsn.readFile('./assets/plate_beautiful.png');
+  const png = person
+  const { body } = await snek.get(png);
+  return new Canvas(634, 675)
+    .setColor('#000000')
+    .addRect(0, 0, 634, 675)
+    .addImage(body, 423, 45, 168, 168)
+    .addImage(body, 426, 382, 168, 168)
+    .addImage(plate, 0, 0, 634, 675)
+    .toBuffer();
+};
+try {
+      const msg = await message.channel.send('Admiring the painting...');
+      const result = await getBeautiful(person.avatarURL({ format:"png", size:256 }));
+      await message.channel.send({ files: [{ attachment: result, name: 'beautiful.jpg' }] });
+      await msg.delete();
+ }
 }
