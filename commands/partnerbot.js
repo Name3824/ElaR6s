3ec819e43569cbdd670c33ec7b1f30fbbcf15941
YoutubeTutorial <35500201+YoutubeTutorial@users.noreply.github.com>
2018-02-14 16:19:19 +0100

    Create partnerbot.js

---
+++ commands/partnerbot.js
@@ -0,0 +1,13 @@
+
+exports.run = (client, msg) => {
+    const Discord = require('discord.js');
+    const emb = new Discord.RichEmbed();
+    msg.channel.startTyping();
+    emb.setColor("#75C0AC");
+    emb.setAuthor(client.user.tag+' 💙', client.user.avatarURL, 'http://mitsuhabot.ml');
+    emb.addField('**Invite Mitsuha!**', '[Click Me](https://discordapp.com/oauth2/authorize?client_id=395546680482725888&scope=bot&permissions=57953281)', true);
+    emb.setImage('https://discordbots.org/api/widget/395546680482725888.svg?usernamecolor=660096&topcolor=ffffff&highlightcolor=FFFFFF&labelcolor=660096&middlecolor=ffffff&datacolor=00080');
+    emb.setFooter('Prefix e!');
+    msg.channel.send({embed:emb});
+    msg.channel.stopTyping();
+}
