ff877f2d772f750096439f30f9d6346ddfd14bbe
elabots <35500201+elabots@users.noreply.github.com>
2018-02-15 10:44:05 +0100

    Update emojilist.js

--- commands/emojilist.js
+++ commands/emojilist.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 exports.run = (client, msg, args) => {
       const emojiList = msg.guild.emojis.map(e => e.toString()).join(" ");
-      const embed = new Discord.RichEmbed()
-      .setColor("#75C0AC")
-      .setTitle("Emoji Of This Server")
-      .setDesciption(emojiList)
-      msg.channel.send({embed});
+    const emb = new Discord.RichEmbed();
+      emb.setColor("#75C0AC");
+      emb.setTitle("Emoji Of This Server");
+      emb.setDesciption(emojiList);
+      msg.channel.send({embed:emb});
 }
