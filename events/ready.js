d5de267ee00f0de54a3b42b9a6262e651440371a
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-16 21:56:57 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 exports.run = (client) => {
     console.log('Preparing...');
     client.user.setActivity(process.env.PLAYING, {type: 'WATCHING'});
+    client.user.setUsername("GreenBlood");
     console.log('Loading users... '+client.users.size+'\nLoading channels... '+client.channels.size+'\nLoading servers... '+client.guilds.size+'\n'+client.user.id+' is successfully online!');
 }
