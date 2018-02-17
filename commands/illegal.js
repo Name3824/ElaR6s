de1cd811de0bd2885712fc48a345846a8bd3ba20
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:50:01 +0100

    Update illegal.js

--- commands/illegal.js
+++ commands/illegal.js
@@ -9,7 +9,7 @@ return message.reply(':x: **Please, specify a thing that trump will make illegal
 if (meow.length > 1) {
 return message.reply(':x: **Max 1 word**'); }
  const emb = new Discord.RichEmbed();
- emb.setColor("#ffee07");
+ emb.setColor("#ffd954");
  emb.setAuthor("Trump has now made " + meow + " illegal!", "http://blog.adsy.me/wp-content/uploads/2016/11/angry-side-face-trump-transparent.png");
  emb.setImage(illegal);
  message.channel.send({embed:emb})
