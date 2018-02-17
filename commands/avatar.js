b0ca6ebcd9421dfa4fa7f6511535cb58858f5625
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 19:12:03 +0100

    Delete avatar.js

--- commands/avatar.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js')
-exports.run = (client, message, args) => {      
-            message.mentions.members.first() !== undefined ? person = message.mentions.members.first() : person = message.member
-            const embed = new Discord.RichEmbed()
-            .setImage(person.user.displayavatarURL({size: 2048}))
-            .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
-            message.channel.send({embed})
-    }
