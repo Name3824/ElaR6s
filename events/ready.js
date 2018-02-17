1410a9c6aa95f6cfce315ea767c3e83daeb287e4
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 19:49:01 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,11 @@
+const Discord = require('discord.js');
+const DBL = require("dblapi.js");
 exports.run = (client) => {
     console.log('Preparing...');
     client.user.setActivity(process.env.PLAYING, {type: 'STREAMING'});
     client.user.setUsername("BartBot");
     console.log('Loading users... '+client.users.size+'\nLoading channels... '+client.channels.size+'\nLoading servers... '+client.guilds.size+'\n'+client.user.id+' is successfully online!');
+   client.dbl = new DBL(process.env.DISCORDBOTLIST, client);
+   client.dbl.postStats(client.guilds.size)
+    .then(() => console.log('Updated discordbots.org stats.'));
 }
