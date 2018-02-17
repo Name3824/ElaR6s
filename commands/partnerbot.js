9c8fb5da5313f05743ab112128bc93603ffc33f4
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:31:09 +0100

    Update partnerbot.js

--- commands/partnerbot.js
+++ commands/partnerbot.js
@@ -3,11 +3,11 @@ exports.run = (client, msg) => {
     const Discord = require('discord.js');
     const emb = new Discord.RichEmbed();
     msg.channel.startTyping();
-    emb.setColor("#75C0AC");
-    emb.setAuthor(client.user.tag+' 💙', client.user.avatarURL, 'http://mitsuhabot.ml');
+    emb.setColor("#ffee07");
+    emb.setAuthor(client.user.tag+' ', client.user.avatarURL, 'http://mitsuhabot.ml');
     emb.addField('**Invite Mitsuha!**', '[Click Me](https://discordapp.com/oauth2/authorize?client_id=395546680482725888&scope=bot&permissions=57953281)', true);
     emb.setImage("https://discordbots.org/api/widget/395546680482725888.svg");
-    emb.setFooter('Prefix e!');
+    emb.setFooter('Prefix b!');
     msg.channel.send({embed:emb});
     msg.channel.stopTyping();
 }
