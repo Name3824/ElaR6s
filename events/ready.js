9c0bf399e70f5e808bb7e0c3f8aa63dbe630458e
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 14:40:17 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,14 @@
+const snekfetch = require('snekfetch');
+const Discord = require('discord.js');
+
 exports.run = (client) => {
+    
+    	snekfetch.post(`https://discordbots.org/api/bots/stats`)
+  .set('Authorization', process.env.DBLORG)
+  .send({ server_count: client.guilds.size })
+  .then(() => console.log('Updated discordbots.org stats.'))
+.catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+    
     console.log('Preparing...');
     client.user.setActivity(process.env.PLAYING, {type: 'STREAMING'});
     client.user.setUsername("BartBot");
