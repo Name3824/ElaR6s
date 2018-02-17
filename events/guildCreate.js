bd785e5d26531fab4d7bfee7791ca7c2ef549012
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 14:34:18 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,9 +2,7 @@ const snekfetch = require('snekfetch');
 const Discord = require('discord.js');
 
 exports.run = (client, guild) => {
-	snekfetch.post(`https://discordbots.org/api/bots/stats`)
-  .set('Authorization', process.env.DISCORDBOTLIST)
-  .send({ server_count: client.guilds.size })
-  console.log('Updated discordbots.org stats.')
-  console.error(`Whoops something went wrong: ${err.body}`)
+    snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+    .set('Authorization', process.env.DISCORDBOTLIST)
+.send({ server_count: client.guilds.size })
 }
