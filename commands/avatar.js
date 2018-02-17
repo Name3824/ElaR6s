efe4a239740c03acec12b890dbbeaefb55d09ea5
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 19:09:45 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {      
             message.mentions.members.first() !== undefined ? person = message.mentions.members.first() : person = message.member
             const embed = new Discord.RichEmbed()
-            .setImage(person.user.avatarURL({size: 2048}))
+            .setImage(person.user.displayavatarURL({size: 2048}))
             .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
             message.channel.send({embed})
     }
