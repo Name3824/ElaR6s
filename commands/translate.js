0cdc674b3e3821bb85de5769f37f9be7fa7cfcc2
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 19:01:28 +0100

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,19 +1,6 @@
-const translate = require('google-translate-api');
-const Discord = require('discord.js');
-
-exports.run = (client, msg, args) => {
-    const embed = new Discord.RichEmbed();
-    const text = args.join(" ");
-    translate(text, {to: 'en'}).then(async function(res) {
-       await msg.channel.startTyping();
-       await embed.setColor('#46BE30');
-       await embed.setDescription(":flag_"+res.from.language.iso+": ➤ *"+text+"*\n:flag_us: ➤ *"+res.text+"*");
-       await embed.setFooter(msg.author.tag, msg.author.avatarURL);
-       await msg.channel.send({embed:embed});
-       await msg.channel.stopTyping();
-   }).catch(async function(err) {
-       await msg.channel.startTyping();
-       await msg.channel.send("**:x: | ERRO**\n```"+err+"```");
-       await msg.channel.stopTyping();
-   });
-}
+  exports.run = async (client, message, args) => {
+    message.mentions.users.first() !== undefined ? person = message.mentions.users.first() : person = message.author
+    const avatar = person.displayAvatarURL({format : 'png'})
+    const pls = await snekfetch.get(`http://api.anidiots.guide/api/triggered/?avatar=${avatar}`).set("token", "Oe3S8aiv7D2gNWqj/oDv");
+    await message.channel.send({ files: [{ attachment: pls.body, name: 'triggered.gif' }] });
+} 
