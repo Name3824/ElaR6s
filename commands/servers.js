fad5caabcde6c7d98b2ba36a58b45cc30ddc0e90
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:52:24 +0100

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
     .setTitle("List Of Servers")
-    .setColor("#ffee07")
+    .setColor("#ffd954")
     .setDescription(client.guilds.map(g=>g.name).join(", "));
   if (embed.description.length > 1998) return message.reply("**Discord API Error**\nCan't send large messages.");
   message.channel.send({embed});
