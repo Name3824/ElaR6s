c978a2490002d0817b7e694b1bdcdd79ffcb7d4b
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 09:09:15 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ exports.run = (client, msg) => {
     emb.setColor("#75C0AC");
     emb.setAuthor(client.user.tag+' invite', client.user.avatarURL, 'http://mitsuhabot.ml');
     emb.addField('**Invite Me!**', '[Click Me](https://discordapp.com/oauth2/authorize?client_id=411860199473938433&scope=bot&permissions=8)', true);
-    emb.setFooter('Prefix e! | e!partnerbot');
+    emb.setFooter('Prefix g! | g!partnerbot');
     msg.channel.send({embed:emb});
     msg.channel.stopTyping();
 }
