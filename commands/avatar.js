d8c0acee84d939ba214be80a8d39c75441b73a13
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 19:03:31 +0100

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,9 @@
+
+const Discord = require('discord.js')
+exports.run = async (client, message, args) => {      
+            message.mentions.members.first() !== undefined ? person = message.mentions.members.first() : person = message.member
+            const embed = new Discord.MessageEmbed()
+            .setImage(person.user.avatarURL({size: 2048}))
+            .setColor(message.guild.member(client.user.id).highestRole.color || 0x00AE86);
+            message.channel.send(embed)
+    }
