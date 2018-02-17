d8930f59d3811c9ec5fd593a8a1f695909ec8165
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:42:07 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 exports.run = (client) => {
     console.log('Preparing...');
-    client.user.setActivity(process.env.PLAYING, {type: 'LISTENING'});
+    /*client.user.setActivity(process.env.PLAYING, {type: 'LISTENING'});*/
+    client.user.setStatus("Online");
+    client.user.setGame("Homer... | b!help", "https://twitch.tv/bart");
     client.user.setUsername("BartBot");
     console.log('Loading users... '+client.users.size+'\nLoading channels... '+client.channels.size+'\nLoading servers... '+client.guilds.size+'\n'+client.user.id+' is successfully online!');
 }
