48b36fef38ecd81770f61cdcff85522181c6cd12
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 18:45:11 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,19 +1,11 @@
-const discord = require("discord.js");
-const translate = require('google-translate-api');
+const { MessageEmbed: RichEmbed } = require('discord.js');
 exports.run = async (client, msg, args) => {
-  const [lang, ...txt] = await args;
-  const string = String(txt)
-  try{
-    await translate(/*"Hello world"*/string, { to: lang }).then(translation => {
-      const embed = new discord.MessageEmbed()
-        .setTitle("Goomba Translator")
-        .setColor(msg.guild.me.roles.highest.color || "0xb22222")
-        .addField(`:flag_${translation.from.language.iso == "en" ? "us" : translation.from.language.iso == "ko" ? "kr" : translation.from.language.iso}: **Original Text**`, txt)
-        .addField(`:flag_${lang == "en" ? "us" : lang == "ko" ? "kr" : lang}: **Translated Text**`, translation.text)
-      msg.channel.send(embed);
-    });
-  } catch(e){
-    console.log(e)
-    msg.channel.send("There was an error whilst translating your text. Be sure you entered correctly the language. (List of supported languages: http://how.evie-banned.me/ezuguwinoy.scala)")
-  }
+  var color = args[0].toLowerCase()
+  const clRX = /^[a-f0-9]+$/g
+  if (!color.match(clRX)) return msg.channel.send("|`X`| The color is in a wrong form")
+  const embed = new RichEmbed()
+    .setTitle(`Color #${color}`)
+    .setImage(`https://dummyimage.com/50x50/${color}/fff&text=+`)
+    .setColor(`0x${color}`)
+  msg.channel.send(embed)
 }
