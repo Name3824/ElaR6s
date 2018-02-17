038ecb6482a50876da2d7afdbff1624728e90111
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:28:02 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,13 @@ exports.run = (client, msg) => {
     const Discord = require('discord.js');
     const emb = new Discord.RichEmbed();
     msg.channel.startTyping();
-    emb.setColor("#75C0AC");
+    emb.setColor("#ffee07");
     emb.setAuthor(client.user.tag+' Commands', client.user.avatarURL, 'http://mitsuhabot.ml');
     emb.addField('**Stats Games 🎮**', '-`lol` -`r6s` -`rocket` -`minecraft` -`cr` -`fortnite` -`s-pokemon`', true);
     emb.addField('**Fun 🎉**', '-`meme` -`battle` -`illegal` -`ascii` -`react`', true);
     emb.addField('**Nsfw 💦**', '-`ass` -`boobs` -`pussy` -`pornhub` -`neko` -`hentai` -`gif`', true);
     emb.addField('**Utility 🤘**', '-`invite` -`partnerbot` -`feedback` -`imdb` -`weather` -`emojilist` -`servers` -`emoteinfo`', true);
-    emb.setFooter('Prefix g!');
+    emb.setFooter('Prefix b!');
     msg.channel.send({embed:emb});
     msg.channel.stopTyping();
     msg.react('414108671166119946');
