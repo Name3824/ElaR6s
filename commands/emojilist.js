4d7187462944191db7237e1da6ec6f8172127ed3
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 11:27:15 +0100

    Update emojilist.js

--- commands/emojilist.js
+++ commands/emojilist.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
 
   const guild = message.guild;
   const embed = new Discord.RichEmbed()
-     .setColor("#75C0AC")
+     .setColor("#ffee07")
     .setAuthor(`${guild.name}'s Custom Emojis`, guild.iconURL)
     .setDescription(guild.emojis.map(e=>e.toString()).join(" "));
   message.channel.send({embed});
