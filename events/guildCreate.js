acf3d88d1a5296d5b179d588229adffdcba54ba0
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-17 14:31:51 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,6 +5,6 @@ exports.run = (client, guild) => {
 	snekfetch.post(`https://discordbots.org/api/bots/stats`)
   .set('Authorization', process.env.DISCORDBOTLIST)
   .send({ server_count: client.guilds.size })
-  console.log('Updated discordbots.org stats.'))
-  console.error(`Whoops something went wrong: ${err.body}`))
+  console.log('Updated discordbots.org stats.')
+  console.error(`Whoops something went wrong: ${err.body}`)
 }
