4848a3a7b3328d9b1e10f4163e4cab3575e67898
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-18 09:42:27 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,16 @@
-const Discord = require('discord.js');
-const DBL = require("dblapi.js");
 exports.run = (client, guild) => {
-client.dbl = new DBL(process.env.DISCORDBOTLIST, client);
-client.dbl.postStats(client.guilds.size)
-  .then(() => console.log('Updated discordbots.org stats.'));
+let users = guild.memberCount
+let bots = guild.members.filter(m=>m.user.bot).size
+let percent = Math.floor(bots / users * 10000) / 100;
+const embed = new Discord.RichEmbed()
+  .setTitle(`${guild.name}`)
+  .setAuthor('Joined Server')
+  .setColor(0xffd954)
+  .setFooter(``)
+  .setTimestamp()
+  .addField('Owner', `${guild.owner.user.tag} (${guild.owner.user.id})`)
+  .addField('Members', `${guild.memberCount - guild.members.filter(m=>m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true)
+  .addField('Percent', `${percent}%`)
+  .addField('ID', `${guild.id}`, true)
+client.channels.get('404280160452608000').send({embed})
 }
