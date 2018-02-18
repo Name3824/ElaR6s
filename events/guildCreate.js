cc353c33aef329a5dca606d4f138c413c4a738d9
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-18 09:46:44 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,15 +2,15 @@ exports.run = (client, guild) => {
 let users = guild.memberCount
 let bots = guild.members.filter(m=>m.user.bot).size
 let percent = Math.floor(bots / users * 10000) / 100;
-const embed = new Discord.RichEmbed()
-  .setTitle(`${guild.name}`)
-  .setAuthor('Joined Server')
-  .setColor(0xffd954)
-  .setFooter(``)
-  .setTimestamp()
-  .addField('Owner', `${guild.owner.user.tag} (${guild.owner.user.id})`)
-  .addField('Members', `${guild.memberCount - guild.members.filter(m=>m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true)
-  .addField('Percent', `${percent}%`)
-  .addField('ID', `${guild.id}`, true)
-client.channels.get('404280160452608000').send({embed})
+ const emb = new Discord.RichEmbed();
+  emb.setTitle(`${guild.name}`);
+  emb.setAuthor('Joined Server');
+  emb.setColor(0xffd954);
+  emb.setFooter(``);
+  emb.setTimestamp();
+  emb.addField('Owner', `${guild.owner.user.tag} (${guild.owner.user.id})`);
+  emb.addField('Members', `${guild.memberCount - guild.members.filter(m=>m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true);
+  emb.addField('Percent', `${percent}%`);
+  emb.addField('ID', `${guild.id}`, true);
+client.channels.get('404280160452608000').send({embed});
 }
