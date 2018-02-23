d5bdc8f1708338e95c14eb75211b25476d8900d6
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-23 12:55:51 +0100

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,17 +2,17 @@ const talkedRecently = new Set();
 const Discord = require('discord.js')
 var figlet = require('figlet');
 exports.run = (client, message, args) => {
-    if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    if (talkedRecently.has(message.author.id)) {
+            message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
     } else {
 
            // the user can type the command ... your command code goes here :)
 
         // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(msg.author.id);
+        talkedRecently.add(message.author.id);
         setTimeout(() => {
           // Removes the user from the set after a minute
-          talkedRecently.delete(msg.author.id);
+          talkedRecently.delete(message.author.id);
         }, 60000);
     }
     
