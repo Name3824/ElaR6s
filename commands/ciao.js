38213e44531e20986c59e662b6af22ab314eac5a
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-23 11:07:05 +0100

    Update ciao.js

--- commands/ciao.js
+++ commands/ciao.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
-
+const urlExpander = require('expand-url');
 exports.run = (client, message) => {
-   var urlExpander = require('expand-url');
     let args = message.content.split(" ").slice(1);
     var url2 = message.content.slice(message.content.indexOf(message.content.split(" ")[1]))
     urlExpander.expand(url2, function(err, longUrl){
@@ -10,5 +9,6 @@ exports.run = (client, message) => {
       .addField('Shortened URL', message.content.slice(message.content.indexOf(message.content.split(" ")[1])))
       .addField('Un-Shortened URL', "<"+ longUrl + ">")
       return message.reply({embed}).then(message => message.delete(30000))
+      }
     }
 }
