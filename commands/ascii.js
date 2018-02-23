bf0ab4dabcdc66030c9fa1a433ca8424c8972908
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-23 12:45:47 +0100

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,7 +2,8 @@ const talkedRecently = new Set();
 const Discord = require('discord.js')
 var figlet = require('figlet');
 exports.run = (client, message, args) => {
-    if (talkedRecently.has(message.author.id))
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Lol test")
   return;
 
 // Adds the user to the set so that they can't talk for 2.5 seconds
