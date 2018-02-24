59af3d70eef169f74b2d55d3c0e67bcf5c94e015
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-24 13:00:44 +0100

    Update botsfordiscord.js

--- commands/botsfordiscord.js
+++ commands/botsfordiscord.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
                 let botid = member.user.id
             req('https://botsfordiscord.com/api/v1/bots/' + botid, (e, r, b)=> {
 						let a = JSON.parse(b)
-					if(a.error === "Not found")  {
+					if(a.error === "This API revision does not exist.")  {
 						const embed = new Discord.RichEmbed()
 						embed.setAuthor(client.user.tag, client.user.avatarURL)
 						embed.setThumbnail(client.user.avatarURL)
