a6b792f40985de4f42738566b7571526daa3d1dd
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-24 20:53:27 +0100

    Update blspace.js

--- commands/blspace.js
+++ commands/blspace.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
                 let botid = member.user.id
             req('https://botlist.space/api/bots/' + botid, (e, r, b)=> {
 						let a = JSON.parse(b)
-					if(a.error === "")  {
+					if(a.error === "{…}")  {
 						const embed = new Discord.RichEmbed()
 						embed.setAuthor(client.user.tag, client.user.avatarURL)
 						embed.setThumbnail(client.user.avatarURL)
