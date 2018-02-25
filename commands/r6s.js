cc92a402ee5ef7b8c284b69f5af71f53ce4264f3
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-25 16:07:12 +0100

    Update r6s.js

--- commands/r6s.js
+++ commands/r6s.js
@@ -1,63 +1,130 @@
-const Discord = require('discord.js');
+
 const RainbowSix = require('rainbowsix-api-node');
 const r6 = new RainbowSix();
-const platforms = ['UPLAY', 'XONE', 'PS4'];
-function platform(query) {
-    if(query == 'UPLAY') return '1';
-    else if(query == 'PS4') return '2';
-    else if(query == 'XONE') return '3';
-}
 
-exports.run = (client, msg, args) => {
-    const q = args.join(" ");
-    const emb = new Discord.RichEmbed();
-    if(q.startsWith('player')) {
-        fullStats = q.replace('player', '').trim();
-        allStats = fullStats.split(" ");
-        if(!allStats[0]) {
-            msg.channel.startTyping();
-            emb.setColor('#F03A17');
-            emb.setTitle('Error.');
-            emb.addField('Platform not defined', 'Try again');
-            emb.setFooter(msg.author.tag, msg.author.avatarURL);
-            msg.channel.send({embed:emb});
-            msg.channel.stopTyping();
-        } else if(!platforms.includes(allStats[0].toUpperCase())) {
-            msg.channel.startTyping();
-            emb.setColor('#F03A17');
-            emb.setTitle('Error.');
-            emb.addField('Invalid platform', 'Try again');
-            emb.setFooter(msg.author.tag, msg.author.avatarURL);
-            msg.channel.send({embed:emb});
-            msg.channel.stopTyping();
-        } else if(!allStats[1]) {
-            msg.channel.startTyping();
-            emb.setColor('#F03A17');
-            emb.setTitle('Error.');
-            emb.addField('Username not defined', 'Try again with');
-            emb.setFooter(msg.author.tag, msg.author.avatarURL);
-            msg.channel.send({embed:emb});
-            msg.channel.stopTyping();
-        } else if(platforms.includes(allStats[0].toUpperCase())) {
-            r6.get(args[1], args[0]).then(data => {
-                await msg.channel.startTyping();
-                await emb.setAuthor('Stats about '+data.player.username, 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-                await emb.setColor("#75C0AC");
-                await emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-                await emb.addField('General', `**Level**: ${data.player.stats.progression.level}`);
-                await emb.setFooter(msg.author.tag, msg.author.avatarURL);
-                await msg.channel.send({embed:emb});
-                await msg.channel.stopTyping();
-            });
+exports.exec = (Bastion, message, args) => {
+  if (!/^(uplay|ps4|xone)$/.test(args[0] = args[0].toLowerCase()))
+  if (!/^[a-zA-Z][\w-. ]{2,14}$/.test(args[1] = args.slice(1).join(' '))) 
+  r6.stats(args[1], args[0]).then(data => {
+    let stats = [
+      {
+        name: 'Player Name',
+        value: data.player.username
+      },
+      {
+        name: 'Level',
+        value: `${data.player.stats.progression.level}`,
+        inline: true
+      },
+      {
+        name: 'XP',
+        value: `${data.player.stats.progression.xp}`,
+        inline: true
+      }
+    ];
+    if (data.player.stats.ranked.has_played) {
+      stats.push(
+        {
+          name: 'Ranked',
+          value: `${args[1]} has played Ranked games for **${(data.player.stats.ranked.playtime / 60 / 60).toFixed(2)}** Hours.`
+        },
+        {
+          name: 'Wins',
+          value: `${data.player.stats.ranked.wins}`,
+          inline: true
+        },
+        {
+          name: 'Losses',
+          value: `${data.player.stats.ranked.losses}`,
+          inline: true
+        },
+        {
+          name: 'Kills',
+          value: `${data.player.stats.ranked.kills}`,
+          inline: true
+        },
+        {
+          name: 'Deaths',
+          value: `${data.player.stats.ranked.deaths}`,
+          inline: true
+        },
+        {
+          name: 'Win/Lose Ratio',
+          value: `${data.player.stats.ranked.wlr}`,
+          inline: true
+        },
+        {
+          name: 'Kill/Death Ratio',
+          value: `${data.player.stats.ranked.kd}`,
+          inline: true
         }
-    } else if(!args[0]) {
-        msg.channel.startTyping();
-        emb.setColor("#ffd954");
-        emb.setAuthor('Correct Usage Of Fortnite Commands', 'https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-        emb.setThumbnail('https://cdn.atr.cloud/monthly_2017_10/FortniteClient-Win64-Shipping_123.ico_256x256.png.9db57869789ecc4d9c5f72c5a9ba9e30.thumb.png.d8d082ccd47b246fc3773e854b1b2ead.png');
-        emb.addField("Search for a player's stats\nUsage:", "b!fortnite player [platform] [username]\n\nValid platforms: `pc, ps4, xbox`");
-        emb.setFooter(msg.author.tag, msg.author.avatarURL);
-        msg.channel.send({embed:emb});
-        msg.channel.stopTyping();
+      );
     }
-}
+    else {
+      stats.push(
+        {
+          name: 'Ranked',
+          value: `${args[1]} has not played any Ranked game.`
+        }
+      );
+    }
+    if (data.player.stats.casual.has_played) {
+      stats.push(
+        {
+          name: 'Casual',
+          value: `${args[1]} has played Casual games for **${(data.player.stats.casual.playtime / 60 / 60).toFixed(2)}** Hours.`
+        },
+        {
+          name: 'Wins',
+          value: `${data.player.stats.casual.wins}`,
+          inline: true
+        },
+        {
+          name: 'Losses',
+          value: `${data.player.stats.casual.losses}`,
+          inline: true
+        },
+        {
+          name: 'Kills',
+          value: `${data.player.stats.casual.kills}`,
+          inline: true
+        },
+        {
+          name: 'Deaths',
+          value: `${data.player.stats.casual.deaths}`,
+          inline: true
+        },
+        {
+          name: 'Win/Lose Ratio',
+          value: `${data.player.stats.casual.wlr}`,
+          inline: true
+        },
+        {
+          name: 'Kill/Death Ratio',
+          value: `${data.player.stats.casual.kd}`,
+          inline: true
+        }
+      );
+    }
+    else {
+      stats.push(
+        {
+          name: 'Casual',
+          value: `${args[1]} has not played any Casual game.`
+        }
+      );
+    }
+    message.channel.send({
+      embed: {
+        color: 0xffffff,
+        title: 'Rainbow 6',
+        url: `https://r6stats.com/stats/${args[0]}/${encodeURIComponent(args[1])}`,
+        fields: stats,
+        thumbnail: {
+          url: 'https://vignette1.wikia.nocookie.net/rainbowsix/images/0/06/Rainbow_(Clear_Background)_logo.png'
+        }
+      }
+    })
+  })
+  });
+};
