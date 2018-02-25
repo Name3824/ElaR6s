65233c056b1f90debda3417b5b1cff8299c8c736
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-25 12:28:46 +0100

    Update ciao.js

--- commands/ciao.js
+++ commands/ciao.js
@@ -8,7 +8,7 @@ exports.run = (client, message) => {
       .setColor(0x2ce88e)
       .addField('Shortened URL', message.content.slice(message.content.indexOf(message.content.split(" ")[1])))
       .addField('Un-Shortened URL', "<"+ longUrl + ">")
-      return message.reply({embed}).then(message => message.delete(30000))}
+      return message.reply({embed}).then(message => message.delete(30000))
       }
     }
 }
