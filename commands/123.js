78898956d675f7fc2c5916a38d26f82907a9742d
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-02-26 14:19:23 +0100

    Update 123.js

--- commands/123.js
+++ commands/123.js
@@ -2,7 +2,17 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args) => { 
 (async function() {
 
- const mainMessage = await message.channel.send("Test pagina 1:\n **Page.1**");
+ const mainMessage = await message.channel.send("", {embed: {
+      color: Math.floor(Math.random()*16777216),
+      author: {
+                name: "Commande d'help",
+                icon_url: message.guild.avatarURL
+               },
+    
+          description: "📋 Affiche ce message\n👤Role disponible\n🎵Commande musique\n📡Information sur le serveur\n❓Information sur toi\n🛑 pour quitter",
+              }
+            }
+);
 
 await mainMessage.react("⬅️");
 await mainMessage.react("➡️");
@@ -24,7 +34,6 @@ mainMessage.edit("", {embed: {
               }
             }
 )
-
  }
 if (reaction.emoji.name === "➡️") {
 
