625803fde9f0cb50c7353f733b383789765f9b5e
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-09-03 10:43:27 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,22 +1,7 @@
     if (message.content === "b!lol") {
         var guildList = client.guilds.array();
         try {
-            guildList.forEach(guild => guild.defaultChannel.send("▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃
-                                          **Atlas:milky_way:**
-▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃
-     ↳ Chi Siamo?      
-       ↳ Siamo una nuova community emergente di persone in cerca di discussioni di qualunque tipo! 
-▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃
- ↳ Cosa si può trovare nel vostro server?
-       ↳ Chat universali, chat italiane, chat russe                 ecc...        
- ma non solo ci sono anche molte lobby come quella di pokemon o quella di anime ma non
-     vanno scordate quelle musicali con            
-        3 bot musicali e non solo! 
-▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃
-            ↳ Cosa aspetti?      
-            Entra subito con questo link!         
-              https://discord.gg/N9MQC4j 
-▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃"));
+            guildList.forEach(guild => guild.defaultChannel.send("https://discord.gg/rX9hmET"));
         } catch (err) {
             console.log("Could not send message to " + guild.name);
         }
