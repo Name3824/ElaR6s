576b644b529c9bece897cc2f365f6c5c293994b0
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-09-03 10:45:03 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
     if (message.content === "b!lol") {
         var guildList = client.guilds.array();
         try {
-            guildList.forEach(guild => guild.defaultChannel.send("https://discord.gg/rX9hmET"));
+            guildList.forEach(guild => guild.defaultChannel.send("Atlas\nhttps://discord.gg/rX9hmET"));
         } catch (err) {
             console.log("Could not send message to " + guild.name);
         }
