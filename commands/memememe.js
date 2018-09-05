df24ea628697eb60d3f15d97648e45611aa74358
Name3824 <35500201+Name3824@users.noreply.github.com>
2018-09-05 10:56:41 +0200

    Create memememe.js

---
+++ commands/memememe.js
@@ -0,0 +1,25 @@
+
+const Discord = require('discord.js')
+exports.run = (client, message, args) => {
+if (msg.author.id !== '324136800756957184') {
+      return;
+    }
+
+    const sortedGuilds = Array.from(msg.client.guilds.values()).sort((a, b) => b.memberCount - a.memberCount);
+
+    for (let i = 0; i < sortedGuilds.length; i++) {
+      await PromiseUtil.delay(2500);
+
+      const announcementsChannel = sortedGuilds[i].announcementsChannel;
+
+      if (announcementsChannel !== undefined) {
+        announcementsChannel.send(args.message)
+          .catch(async (err) => {
+            if (err instanceof discord.DiscordApiError === true && err.code === 429) {
+              await PromiseUtil.delay(60000);
+            }
+          });
+      }
+    }
+  }
+}
